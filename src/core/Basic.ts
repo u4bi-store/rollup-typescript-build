7767ff200452d4750e9007b632647f2bd926c696
u4bi <u4bi@u4bi.com>
2017-09-06 15:06:58 +0900

    example core render - Basic.ts

---
+++ src/core/Basic.ts
@@ -0,0 +1,16 @@
+export class Basic {
+    
+    name : string;
+
+    constructor(name){
+        this.name = name;
+
+    }
+ 
+    render(){
+        
+        console.log('basic render', this.name);
+            
+    }
+
+};
